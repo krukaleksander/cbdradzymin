e81ccbcf576c10844ab75946337f43fed4acb2b6
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-01-07 06:43:35 +0100

    add grapgic to legality

--- src/pages/Legality.js
+++ src/pages/Legality.js
@@ -1,15 +1,10 @@
 import React from "react";
-
+import lawImg from "../images/weed_law.png";
 function Legality() {
   return (
     <div className="legality-container">
       <h1>Cz produkty z CBD są rzeczywiście legalne?</h1>
-      <p className="legality-container__par">
-        Dla niecierpliwych przekazujemy od razu odpowiedź : <span>tak.</span>
-      </p>
-      <p className="legality-container__par">
-        Uzasadnienie możecie przeczytać poniżej.
-      </p>
+      <img src={lawImg} alt="" />
       <p className="legality-container__par">
         Wielość odmian konopi może zaskoczyć osoby, które do tej pory nie
         zgłębiały wiedzy na temat tych roślin. Choć duża część z nich faktycznie
@@ -26,16 +21,19 @@ function Legality() {
         ilość THC umożliwia obecny stan prawny:
       </p>
       <p className="legality-container__par legality-container__par--super">
-        Zgodnie z art. 4 pkt 5 ustawy o przeciwdziałaniu narkomanii konopie
-        włókniste, to rośliny z gatunku konopie siewne (Cannabis sativa L.), w
-        których suma zawartości delta-9-tetrahydrokannabinolu (THC) oraz kwasu
-        tetrahydrokannabinolowego (kwas-THC) w kwiatowych lub owocujących
-        wierzchołkach roślin, z których nie usunięto żywicy, nie przekracza 0,20
-        % w przeliczeniu na sucha masę. Z kolei zgodnie z art. 4 pkt 37 ziele
-        konopi innych niż włókniste oznacza każdą naziemną część rośliny konopi
-        (pojedynczą lub w mieszaninie), z wyłączeniem nasion, zawierającą
-        powyżej 0,20% sumy delta-9tetrahydrokannabinolu (THC) oraz kwasu
-        tetrahydrokannabinolowego (kwas-THC).
+        <span className="paragraph">§</span>
+        <span className="text">
+          Zgodnie z art. 4 pkt 5 ustawy o przeciwdziałaniu narkomanii konopie
+          włókniste, to rośliny z gatunku konopie siewne (Cannabis sativa L.), w
+          których suma zawartości delta-9-tetrahydrokannabinolu (THC) oraz kwasu
+          tetrahydrokannabinolowego (kwas-THC) w kwiatowych lub owocujących
+          wierzchołkach roślin, z których nie usunięto żywicy, nie przekracza
+          0,20 % w przeliczeniu na sucha masę. Z kolei zgodnie z art. 4 pkt 37
+          ziele konopi innych niż włókniste oznacza każdą naziemną część rośliny
+          konopi (pojedynczą lub w mieszaninie), z wyłączeniem nasion,
+          zawierającą powyżej 0,20% sumy delta-9tetrahydrokannabinolu (THC) oraz
+          kwasu tetrahydrokannabinolowego (kwas-THC).
+        </span>
       </p>
       <p className="legality-container__par">
         Z powyższego wynika wprost, że kluczowym rozstrzygnięciem jest tutaj
