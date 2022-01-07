969927ed492650354439dab4350e45a1693f5e9c
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-01-07 07:18:30 +0100

    add graphic to information, remove some content

--- src/pages/Information.js
+++ src/pages/Information.js
@@ -1,81 +1,82 @@
 import React from "react";
-
+import Men from "../images/arystokrata.png";
+import Leaf from "../images/list_leaf.png";
 function Information() {
   return (
     <div className="information-container">
-      <h2>
-        Konopie w Polsce używane były od wieków, ślad ich można znaleźć nawet w
-        literaturze:
-      </h2>
-      <p className="information-container__poem">
-        Protazy, choć człek śmiały, uczuł nieco strachu:
-      </p>
-      <p className="information-container__poem">
-        Bo przypomniał z samego rośliny zapachu
-      </p>
-      <p className="information-container__poem">
-        Różne swoje dawniejsze woźnieńskie przypadki,
-      </p>
-      <p className="information-container__poem">
-        Jedne po drugich, biorąc konopie na świadki:{" "}
-      </p>
-      <p className="information-container__poem">
-        Jako raz zapozwany szlachcic z Telsz, ǲindolet,
-      </p>
-      <p className="information-container__poem">
-        Rozkazał mu, oparłszy o piersi pistolet,{" "}
-      </p>
-      <p className="information-container__poem">
-        Wleźć pod stół i ów pozew psim głosem odszczekać,{" "}
-      </p>
-      <p className="information-container__poem">
-        Że Woźny musiał co tchu w konopie uciekać.
-      </p>
-      <p className="information-container__author">
-        Adam Mickiewicz - Pan Tadeusz
-      </p>
-      <h2>Pozwalając sobie na jeszcze jeden przykład:</h2>
-      <p className="information-container__plot">
-        Radziłem mu – mówił – iżby siemię konopne w kieszeni nosił i po trochu
-        spożywał. To tak ci się do tego przyzwyczaił, że teraz coraz to ziarno
-        wyjmie, wrzuci do gęby, rozgryzie, miazgę zje, a łuskwinę wyplunie. W
-        nocy, jak się obudzi, także to czyni. Od tej pory tak mu się dowcip
-        zaostrzył, że i najbliżsi go nie poznają. – Jakże to? – pytał starosta
-        kałuski. – Bo w konopiach oleum się znajduje, przez co i w głowie
-        jedzącemu go przybywa. – Bodajże waszą mość! – rzekł jeden z
-        pułkowników. – Toż w brzuchu oleju przybywa, nie w głowie. – Est modus
-        in rebus! – rzecze na to Zagłoba – trzeba co najwięcej wina pić: oleum,
-        jako lżejsze, zawsze będzie na wierzchu, wino zaś, które i bez tego
-        idzie do głowy, poniesie ze sobą każdą cnotliwą substancję.
-      </p>
-      <p className="information-container__author">
-        Henryk Sienkiewicz – Potop
-      </p>
-      <p className="information-container__par">
-        Obecny stan prawny zezwala na korzystanie z medycznej marihuany osobom,
-        które posiadają wystawioną przez lekarza receptę. Okazuje się jednak, że
-        za wiele zdrowotnych właściwości konopi odpowiadają inne substancje w
-        niej zawarte niż THC - w Polsce jest regulowane prawnie, więcej o tym w
-        zakładce Legalność.
-      </p>
+      <div className="information-container__div">
+        <img src={Men} alt="" />
+        <div>
+          <p className="information-container__plot">
+            Radziłem mu – mówił – iżby siemię konopne w kieszeni nosił i po
+            trochu spożywał. To tak ci się do tego przyzwyczaił, że teraz coraz
+            to ziarno wyjmie, wrzuci do gęby, rozgryzie, miazgę zje, a łuskwinę
+            wyplunie. W nocy, jak się obudzi, także to czyni. Od tej pory tak mu
+            się dowcip zaostrzył, że i najbliżsi go nie poznają. – Jakże to? –
+            pytał starosta kałuski. – Bo w konopiach oleum się znajduje, przez
+            co i w głowie jedzącemu go przybywa. – Bodajże waszą mość! – rzekł
+            jeden z pułkowników. – Toż w brzuchu oleju przybywa, nie w głowie. –
+            Est modus in rebus! – rzecze na to Zagłoba – trzeba co najwięcej
+            wina pić: oleum, jako lżejsze, zawsze będzie na wierzchu, wino zaś,
+            które i bez tego idzie do głowy, poniesie ze sobą każdą cnotliwą
+            substancję.
+          </p>
+          <p className="information-container__author">
+            Henryk Sienkiewicz – Potop
+          </p>
+        </div>
+      </div>
       <h2>
         Suplementacja CBD może wpłynąć pozytywnie na osoby mające problem z:
       </h2>
       <ul>
-        <li>depresją,</li>
-        <li>atakami paniki,</li>
-        <li>nerwicami,</li>
-        <li>artretyzmem,</li>
-        <li>problemami ze snem,</li>
-        <li>stresem.</li>
+        <li>
+          <span>
+            <img src={Leaf} alt="" />
+          </span>
+          depresją,
+        </li>
+        <li>
+          <span>
+            <img src={Leaf} alt="" />
+          </span>
+          atakami paniki,
+        </li>
+        <li>
+          <span>
+            <img src={Leaf} alt="" />
+          </span>
+          nerwicami,
+        </li>
+        <li>
+          <span>
+            <img src={Leaf} alt="" />
+          </span>
+          artretyzmem,
+        </li>
+        <li>
+          <span>
+            <img src={Leaf} alt="" />
+          </span>
+          problemami ze snem,
+        </li>
+        <li>
+          <span>
+            <img src={Leaf} alt="" />
+          </span>
+          stresem.
+        </li>
       </ul>
       <p className="information-container__par">
         Ponadto CBD oddziaływuje także na kondycję skóry i może pomóc w walce z
-        trądzikiem. Inne badania wykazują, że CBD ma potencjał działania
-        przeciwcukrzycowego ,przeciwnowotworowego, neuroprotekcyjnego, czy
-        ochronnego w stosunku do układu krwionośnego, . Wskazuje się coraz
-        częściej, że CBD może być też pomocny w terapii uzależnień, boreliozy,
-        endometriozy.
+        trądzikiem. Inne{" "}
+        <span>
+          badania wykazują, że CBD ma potencjał działania przeciwcukrzycowego,
+          przeciwnowotworowego, neuroprotekcyjnego, czy ochronnego w stosunku do
+          układu krwionośnego.
+        </span>{" "}
+        Wskazuje się coraz częściej, że CBD może być też pomocny w terapii
+        uzależnień, boreliozy, endometriozy.
       </p>
     </div>
   );
