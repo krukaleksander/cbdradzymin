ac1714c74a27f0474f72f1466d9deccbd03a52aa
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-01-07 05:51:12 +0100

    legality ok

--- src/pages/Legality.js
+++ src/pages/Legality.js
@@ -1,7 +1,50 @@
 import React from "react";
 
 function Legality() {
-  return <h1>Legalność</h1>;
+  return (
+    <div className="legality-container">
+      <h1>Cz produkty z CBD są rzeczywiście legalne?</h1>
+      <p className="legality-container__par">
+        Dla niecierpliwych przekazujemy od razu odpowiedź : <span>tak.</span>
+      </p>
+      <p className="legality-container__par">
+        Uzasadnienie możecie przeczytać poniżej.
+      </p>
+      <p className="legality-container__par">
+        Wielość odmian konopi może zaskoczyć osoby, które do tej pory nie
+        zgłębiały wiedzy na temat tych roślin. Choć duża część z nich faktycznie
+        ma działanie narkotyczne, to jednak pewna grupa nie. Dzieje się tak
+        dlatego, że
+        <span> w konopiach występuje około 113 substancji aktywnych</span>, ta
+        odpowiedzialna za działanie narkotyczne to tetrahydrokannabinol – w
+        skrócie THC. Pewna grupa odmian konopii posiada tą substancję w tak
+        małych stężeniach, że w ogóle nie wpływa narkotycznie na organizm
+        ludzki.
+      </p>
+      <p className="legality-container__par">
+        Korzystanie z dobrodziejstw konopi zawierających dużo CBD a znikomą
+        ilość THC umożliwia obecny stan prawny:
+      </p>
+      <p className="legality-container__par legality-container__par--super">
+        Zgodnie z art. 4 pkt 5 ustawy o przeciwdziałaniu narkomanii konopie
+        włókniste, to rośliny z gatunku konopie siewne (Cannabis sativa L.), w
+        których suma zawartości delta-9-tetrahydrokannabinolu (THC) oraz kwasu
+        tetrahydrokannabinolowego (kwas-THC) w kwiatowych lub owocujących
+        wierzchołkach roślin, z których nie usunięto żywicy, nie przekracza 0,20
+        % w przeliczeniu na sucha masę. Z kolei zgodnie z art. 4 pkt 37 ziele
+        konopi innych niż włókniste oznacza każdą naziemną część rośliny konopi
+        (pojedynczą lub w mieszaninie), z wyłączeniem nasion, zawierającą
+        powyżej 0,20% sumy delta-9tetrahydrokannabinolu (THC) oraz kwasu
+        tetrahydrokannabinolowego (kwas-THC).
+      </p>
+      <p className="legality-container__par">
+        Z powyższego wynika wprost, że kluczowym rozstrzygnięciem jest tutaj
+        stężenie THC w suszu czy też olejku. Produkty przez nas sprzedawane
+        zostały przebadane przez niezależne laboratoria, wyniki wykazały, że
+        produkty w naszym sklepie nie przekraczają stężenia wskazanego ustawą.
+      </p>
+    </div>
+  );
 }
 
 export default Legality;
